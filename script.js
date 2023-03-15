c8da56002f70233083054d92e1c570d0a617f529
SrFreitass <guilhermefreitasdonascimento67@gmail.com>
2023-03-15 03:15:58 -0400

    Melhoria no código

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
+
+// Adivinhe o número
+
 const number = Math.floor(Math.random() * 11)
-const input = document.querySelector('.input')
-const button = document.querySelector('.btn')
+const input = document.querySelector('#input')
+const button = document.querySelector('#btn')
 console.log(number)
 
 button.addEventListener('click', () => {
     const inputValue = input.value
     const inputNumber = Number(inputValue)
-    const paragraphbigger = document.querySelector('#bigger')
-    const paragraphsmaller = document.querySelector('#smaller')
-    const numberMystery = document.querySelector('#numberMystery')
+    const paragraph = document.querySelector('#paragraph')
     if (number === inputNumber){
-        numberMystery.classList.toggle('correct')
-
-    } else if (number < inputNumber) {
-        paragraphbigger.classList.toggle('bigger')
-        console.log('O número que você inseriu é menor do que o número misterioso')
-    } else {
-        paragraphsmaller.classList.toggle('smaller')
-        console.log('O número que você inseriu é maior do que o número misterioso')
+        paragraph.innerText = 'Acertou, meus parabéns!'
+    }else if (inputValue == '') {
+        paragraph.innerText = 'Insira um número real ❌'
+    }else if (number < inputNumber) {
+        paragraph.innerText = 'Seu numero é maior do que o misterioso'
+    }else {
+        paragraph.innerText = 'Seu numero é menor do que o misterioso'
     }
+})
+
+// Sound
+
+const playerMusic = document.querySelector('#player-music')
+const buttonPlay = document.querySelector('#sound-off')
+const buttonPause = document.querySelector('#sound-on')
+
+buttonPlay.addEventListener('click', () => {
+    playerMusic.pause()
+})
+
+buttonPause.addEventListener('click', () => {
+    playerMusic.play()
 })
